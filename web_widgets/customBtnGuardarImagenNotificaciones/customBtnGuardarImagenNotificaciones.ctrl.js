56201cc6ff61ec9a1630d377b64c8f377f3bdb50
Arturo Zamorano <arturo.zamorano.sainz@gmail.com>
2020-11-12 00:06:05 -0700

    modificacion de catalogos restantes

---
+++ web_widgets/customBtnGuardarImagenNotificaciones/customBtnGuardarImagenNotificaciones.ctrl.js
@@ -0,0 +1,133 @@
+function PbButtonCtrl($scope, $http) {
+    'use strict';
+    var vm = this;
+    this.isArray = Array.isArray;
+    $scope.loading = false;
+    
+    $scope.sendData = function() {
+        if ($scope.loading === false) {
+            if($scope.properties.isModificacion === false){
+                 
+            }else{
+                $("#loading").modal("show");
+                $scope.loading = true;
+                debugger
+                $scope.properties.contenido[$scope.properties.index].tipoCorreo = $scope.properties.nuevosValores[0].tipoCorreo;
+                $scope.properties.contenido[$scope.properties.index].codigo = $scope.properties.nuevosValores[0].codigo;
+                $scope.properties.contenido[$scope.properties.index].descripcion = $scope.properties.nuevosValores[0].descripcion;
+                $scope.properties.contenido[$scope.properties.index].tipoDocumento = $scope.properties.nuevosValores[0].tipoDocumento;
+                $scope.properties.contenido[$scope.properties.index].nombreImagenHeader = $scope.properties.nuevosValores[0].nombreImagenHeader;
+                $scope.properties.contenido[$scope.properties.index].anguloImagenHeader = $scope.properties.nuevosValores[0].anguloImagenHeader;
+                $scope.properties.contenido[$scope.properties.index].nombreImagenFooter = $scope.properties.nuevosValores[0].nombreImagenFooter;
+                $scope.properties.contenido[$scope.properties.index].montoAspiranteForaneoDolares = $scope.properties.nuevosValores[0].montoAspiranteForaneoDolares;
+                $scope.properties.contenido[$scope.properties.index].anguloImagenFooter = $scope.properties.nuevosValores[0].anguloImagenFooter;
+                $scope.properties.nuevosValores = [];
+                $scope.asignarTarea()
+            }
+        
+        } else {
+            console.log("click doble");
+        }
+    }
+
+    $scope.asignarTarea = function() {
+        var req = {
+            method: "PUT",
+            url: "/bonita/API/bpm/humanTask/" + $scope.properties.taskId,
+            data: angular.copy({ "assigned_id": "" })
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                redireccionarTarea();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+               // notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    function redireccionarTarea() {
+        var req = {
+            method: "PUT",
+            url: "/bonita/API/bpm/humanTask/" + $scope.properties.taskId,
+            data: angular.copy({ "assigned_id": $scope.properties.userId })
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                $scope.submitTask();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                //notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    $scope.submitTask = function() {
+        
+        var req = {
+            method: "POST",
+            url: "/bonita/API/bpm/userTask/" + $scope.properties.taskId + "/execution?assign=false",
+            data: angular.copy($scope.properties.dataToSend)
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                console.log("$scope.properties.dataToSend");
+                console.log($scope.properties.dataToSend);
+                $scope.getConsulta();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+               // notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    $scope.getConsulta = function() {
+        var req = {
+            method: "GET",
+            url: $scope.properties.urlConsulta
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                //console.log("data");
+                //console.log(data);
+                $scope.properties.contenido = data;
+                $scope.getObjTaskInformation();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                //notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    $scope.getObjTaskInformation = function() {
+        var req = {
+            method: "GET",
+            url: $scope.properties.urlTaskInformation
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                $scope.properties.objTaskInformation = data;
+                $scope.loading = false;
+                $("#loading").modal("hide");
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+               // notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+}
---
+++ web_widgets/customTablaCatalogos/customTablaCatalogos.ctrl.js
@@ -0,0 +1,177 @@
+function PbTableCtrl($scope, $http, modalService) {
+
+    this.isArray = Array.isArray;
+    $scope.loading = false;
+
+    this.isClickable = function() {
+        return $scope.properties.isBound('selectedRow');
+    };
+
+    this.selectRow = function(row) {
+        if (this.isClickable()) {
+            //$scope.properties.selectedRow = row;
+            console.log($scope.properties.selectedRow);
+        }
+    };
+    
+    
+    this.isSelected = function(row) {
+        return angular.equals(row, $scope.properties.selectedRow);
+    }
+
+
+   $scope.modificarData = function(row, index) {
+            closeModal();
+            openModal("modalInputs");
+            $scope.properties.selectedToModificate = [{"persistenceId_string" : "","clave" : "","descripcion" : "","usuarioCreacion" : "","isEliminado" : false,"fechaCreacion": "","montoAspiranteLocal": "","montoAspitanteForaneo": "","montoAspiranteLocalDolares": "","montoAspiranteForaneoDolares": "","instruccionesDePago": "","textoDescriptivoPagoDeshabilitado": "","deshabilitarPagoDeExamenDeAdmision": ""}];
+            $scope.properties.selectedToModificate[0].clave = $scope.properties.contenido[index].clave;
+ 			$scope.properties.selectedToModificate[0].descripcion = $scope.properties.contenido[index].descripcion;
+ 			$scope.properties.selectedToModificate[0].fechaCreacion = $scope.properties.contenido[index].fechaCreacion;
+ 			$scope.properties.selectedToModificate[0].isEliminado = $scope.properties.contenido[index].isEliminado;
+ 			$scope.properties.selectedToModificate[0].usuarioCreacion = $scope.properties.contenido[index].usuarioCreacion;
+ 			$scope.properties.selectedToModificate[0].montoAspiranteLocal = $scope.properties.contenido[index].montoAspiranteLocal;
+ 			$scope.properties.selectedToModificate[0].montoAspitanteForaneo = $scope.properties.contenido[index].montoAspitanteForaneo;
+ 			$scope.properties.selectedToModificate[0].montoAspiranteLocalDolares = $scope.properties.contenido[index].montoAspiranteLocalDolares;
+ 			$scope.properties.selectedToModificate[0].montoAspiranteForaneoDolares = $scope.properties.contenido[index].montoAspiranteForaneoDolares;
+ 			$scope.properties.selectedToModificate[0].instruccionesDePago = $scope.properties.contenido[index].instruccionesDePago;
+ 			$scope.properties.selectedToModificate[0].textoDescriptivoPagoDeshabilitado = $scope.properties.contenido[index].textoDescriptivoPagoDeshabilitado;
+ 			$scope.properties.selectedToModificate[0].deshabilitarPagoDeExamenDeAdmision = $scope.properties.contenido[index].deshabilitarPagoDeExamenDeAdmision;
+            $scope.properties.isModificacion = true;
+            $scope.properties.index = index;
+            $scope.properties.mostrarPantallaEditar = true
+            console.log( $scope.properties.selectedToModificate);
+    }
+    
+    function openModal(modalId) {
+        modalService.open(modalId);
+    }
+
+    function closeModal() {
+        modalService.close();
+    }
+  
+    $scope.deleteData = function(row, index) {
+
+        if ($scope.loading == false) {
+            $("#loading").modal("show");
+            $scope.loading = true;
+            $scope.properties.contenido[index].isEliminado = true;
+            console.log(row);
+            $scope.asignarTarea()
+        } else {
+            console.log("click doble");
+        }
+    }
+
+    $scope.sendData = function(row, index) {
+
+        if ($scope.loading == false) {
+            $("#loading").modal("show");
+            console.log(index)
+            $scope.loading = true;
+            $scope.properties.contenido[index].isEnabled = !$scope.properties.contenido[index].isEnabled
+            console.log(row);
+            $scope.asignarTarea()
+        } else {
+            console.log("click doble");
+        }
+    }
+
+    $scope.asignarTarea = function() {
+        var req = {
+            method: "PUT",
+            url: "/bonita/API/bpm/humanTask/" + $scope.properties.taskId,
+            data: angular.copy({ "assigned_id": "" })
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                redireccionarTarea();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    function redireccionarTarea() {
+        var req = {
+            method: "PUT",
+            url: "/bonita/API/bpm/humanTask/" + $scope.properties.taskId,
+            data: angular.copy({ "assigned_id": $scope.properties.userId })
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                $scope.submitTask();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    $scope.submitTask = function() {
+        console.log($scope.properties.dataToSend)
+        var req = {
+            method: "POST",
+            url: "/bonita/API/bpm/userTask/" + $scope.properties.taskId + "/execution?assign=false",
+            data: angular.copy($scope.properties.dataToSend)
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                $scope.getConsulta();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    $scope.getConsulta = function() {
+        var req = {
+            method: "GET",
+            url: $scope.properties.urlConsulta
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                $scope.properties.contenido = [];
+                $scope.properties.contenido = data;
+                $scope.getObjTaskInformation();
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+
+    $scope.getObjTaskInformation = function() {
+        var req = {
+            method: "GET",
+            url: $scope.properties.urlTaskInformation
+        };
+
+        return $http(req)
+            .success(function(data, status) {
+                $scope.properties.objTaskInformation = data;
+                $scope.loading = false;
+                $("#loading").modal("hide");
+            })
+            .error(function(data, status) {
+                $("#loading").modal("hide");
+                $scope.loading = false;
+                notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+            })
+            .finally(function() {});
+    }
+}
