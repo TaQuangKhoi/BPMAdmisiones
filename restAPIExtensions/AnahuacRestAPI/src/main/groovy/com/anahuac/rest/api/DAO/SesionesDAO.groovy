360724ad68debaa5276be68bb3e4ab9d2162becd
Juan Esquer <jusqueresquer@gmail.com>
2021-08-04 14:36:48 -0700

    actualizaci[on develop

--- restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/DAO/SesionesDAO.groovy
+++ restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/DAO/SesionesDAO.groovy
@@ -6723,7 +6723,7 @@ class SesionesDAO {
 			}
 			try {
 				TimeZone tz = TimeZone.getTimeZone("UTC")
-				DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SSS") // Quoted "Z" to indicate UTC, no timezone offset
+				DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS") // Quoted "Z" to indicate UTC, no timezone offset
 				df.setTimeZone(tz)
 				String nowAsISO = df.format(new Date())
 				pstm = con.prepareStatement("SELECT idbanner,escala,puntuacion FROM resultadoinvp where idbanner=? and sesiones_pid=?")
