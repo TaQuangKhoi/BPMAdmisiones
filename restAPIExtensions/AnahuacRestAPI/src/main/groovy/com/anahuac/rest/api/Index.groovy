8fe35ae4a8661b26cb651bcb1a368f2a366a7b67
Jesus <40417090+workchon@users.noreply.github.com>
2021-08-19 14:02:44 -0700

    actualizacion del rest

--- restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/Index.groovy
+++ restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/Index.groovy
@@ -1541,6 +1541,14 @@ class Index implements RestApiController {
 						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
 					}
 					break;
+				case "selectAspirantesPsicometrico":
+					result = psiDAO.selectAspirantesPsicometrico(parameterP, parameterC, jsonData, context)
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+					break;
 				case "createOrUpdateRestaurarRechazoLRoja":
 					result = hDAO.createOrUpdateRestaurarRechazoLRoja(parameterP, parameterC, jsonData, context);
 					/*result = new Result();
