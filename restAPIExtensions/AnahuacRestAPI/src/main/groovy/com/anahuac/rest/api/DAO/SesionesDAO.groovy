42cfc1a82f04f22482069d831ccdbfc43b4870e7
jusquer2 <74383444+jusquer2@users.noreply.github.com>
2021-09-10 10:59:23 -0700

    empty idbanner no se debe de ver

--- restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/DAO/SesionesDAO.groovy
+++ restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/DAO/SesionesDAO.groovy
@@ -6396,7 +6396,7 @@ class SesionesDAO {
 		try {
 			List < Map < String, Object >> rows = new ArrayList < Map < String, Object >> ();
 			closeCon = validarConexion()
-			pstm = con.prepareStatement("SELECT correoelectronico,dt.idbanner, cr.clave from solicituddeadmision sda inner join detallesolicitud dt on dt.caseid::bigint=sda.caseid inner join catresidencia cr on dt.catresidencia_pid=cr.persistenceid where sda.catcampus_pid=? order by sda.persistenceid desc limit 100 ")
+			pstm = con.prepareStatement("SELECT correoelectronico,dt.idbanner, cr.clave from solicituddeadmision sda inner join detallesolicitud dt on dt.caseid::bigint=sda.caseid inner join catresidencia cr on dt.catresidencia_pid=cr.persistenceid where sda.catcampus_pid=? AND (dt.idbanner is not null or dt.idbanner!='') order by sda.persistenceid desc limit 100 ")
 			pstm.setLong(1, campus_pid)
 			rs = pstm.executeQuery()
 			rows = new ArrayList < Map < String, Object >> ();
