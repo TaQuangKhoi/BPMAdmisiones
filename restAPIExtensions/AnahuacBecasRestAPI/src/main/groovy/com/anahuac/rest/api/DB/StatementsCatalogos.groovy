b133f8d7157fd2e78745ff026a7b72c42bc8bb7f
Jose Carlos Felix Ibarra <josecarlosfelixibarra@gmail.com>
2021-12-31 12:26:59 -0700

    Se avanzo el formulario de solicitud de apoyo educativo aspirante

--- restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/DB/StatementsCatalogos.groovy
+++ restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/DB/StatementsCatalogos.groovy
@@ -1,6 +1,13 @@
 package com.anahuac.rest.api.DB
 
 class StatementsCatalogos {
+	//INSERTS
+	//EJEMPLO insert autoincrementado: 		INSERT tabla (persistenceid) VALUES ((SELECT  ISNULL(MAX(persistenceId)+1, 1) FROM tabla));
+	
+	public static final String INSERT_CATTIPOMONEDA = "INSERT INTO CATTIPOMONEDA (PERSISTENCEID, CLAVE, DESCRIPCION, FECHACREACION, ISELIMINADO, PERSISTENCEVERSION, USUARIOCREACION) VALUES ((SELECT  ISNULL(max(persistenceId)+1, 1) FROM CATTIPOMONEDA), ?, ?, now(), false, 1, ?);";
+	
+	//GETS
+	
 	public static final String GET_CATGENERICO = "SELECT PERSISTENCEID, CLAVE, DESCRIPCION, FECHACREACION, ISELIMINADO, PERSISTENCEVERSION, USUARIOCREACION FROM [CATALOGO] [WHERE] [ORDERBY] [LIMITOFFSET]";
 	
 	public static final String GET_COUNT_CATGENERICO = "SELECT COUNT(persistenceid) as registros FROM [CATALOGO] [WHERE] ";
