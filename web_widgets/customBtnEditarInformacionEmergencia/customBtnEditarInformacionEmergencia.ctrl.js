b6726f40e7bdb5786a5c97bc0528108f48a17ed7
Jesus Daniel <danielibarra38@hotmail.com>
2022-01-05 12:12:31 -0700

    Se solucionaron problemas

---
+++ web_widgets/customBtnEditarInformacionEmergencia/customBtnEditarInformacionEmergencia.ctrl.js
@@ -0,0 +1,14 @@
+ function PbButtonCtrl($scope, modalService, $http, blockUI, $q, $filter) {
+        var vm = this;
+
+          this.action = function action() {
+            if($scope.properties.banderaEdicion == true){
+                $scope.properties.banderaEdicion = false;
+                
+            }else if($scope.properties.banderaEdicion == false){
+                $scope.properties.banderaEdicion = true
+            }
+              
+          };
+
+        }
