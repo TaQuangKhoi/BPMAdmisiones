d7a9da1ece47d60646c69c44435737b0004909d6
Jesus <40417090+workchon@users.noreply.github.com>
2022-02-16 13:43:01 -0700

    correcion consulta

--- web_widgets/customCustomBonitaLayoutMenuFront/customCustomBonitaLayoutMenuFront.ctrl.js
+++ web_widgets/customCustomBonitaLayoutMenuFront/customCustomBonitaLayoutMenuFront.ctrl.js
@@ -310,7 +310,7 @@ function WidgetlivingApplicationMenuController($scope, $http, $window, $location
                             setApplication();
                         }
                     } else {
-                        doRequest("GET", "../API/extension/RegistroRest?url=archivedActivity&p=0&c=10&caseid=" + datos1[0].caseId + "&f=state=aborted", null, null, function(datos2) {
+                        doRequest("GET", "../API/extension/RegistroRest?url=archivedHumanTask&p=0&c=10&caseid=" + datos1[0].caseId + "&f=state=aborted", null, null, function(datos2) {
                             for (let index = 0; index < datos2.length; index++) {
                                 const element = datos2[index].displayName;
                                 if (true) {
