6249ded78870fe66a004b699de715a13e2c259b7
José García <josegarciar177@gmail.com>
2022-02-18 17:55:40 -0700

    se hizo el getcattieneshijos

--- restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/IndexGET.groovy
+++ restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/IndexGET.groovy
@@ -134,6 +134,15 @@ class IndexGET implements RestApiController {
 						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
 					}
 					break;
+				case "getCatTienesHijos":
+					result = new SolicitudDeAdmisionDAO().CatalogosDAO();
+					responseBuilder.withMediaType("application/json");
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result.data).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+					break;
 				default:
 					result = notFound(url);
 					if (result.isSuccess()) {
