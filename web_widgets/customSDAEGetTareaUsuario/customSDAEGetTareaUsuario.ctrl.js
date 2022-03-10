b2a074b72e1a22e147bdb2e217f541c11d0375cb
José García <josegarciar177@gmail.com>
2022-03-09 17:11:24 -0700

    Se hicieron cambios importantes  al diagrama, se agrego una actividad que cambie el estatus de la solicitud, se modifico el formulario de inicio de solicitud,  se agregaron las fotos básicas al proceso, se creo el layout para  SDAE aspirante, se mapeo una pagina para cuando la soliicitud se temrina

--- web_widgets/customSDAEGetTareaUsuario/customSDAEGetTareaUsuario.ctrl.js
+++ web_widgets/customSDAEGetTareaUsuario/customSDAEGetTareaUsuario.ctrl.js
@@ -1,22 +1,30 @@
 function ($scope, $http) {
 
-    function getCurrentTaskId(){
-        $http.get($scope.properties.url).success((data)=>{
-            if(data.length){
+    function getCurrentTaskId() {
+        $http.get($scope.properties.url).success((data) => {
+            if (data.length) {
                 $scope.properties.taskId = data[0].id;
                 $scope.properties.caseId = data[0].caseId;
                 getCurrentContext();
             }
-        }).error((err)=>{
-            swal("Error","Error al obtener las tareas asignadas al usuario. " + err,"error");
+        }).error((err) => {
+            swal("Error", "Error al obtener las tareas asignadas al usuario. " + err, "error");
         });
     }
-    
-    function getCurrentContext(){
-        $http.get($scope.properties.urlContext).success((data)=>{
-            if(data.solicitudApoyoEducativo_ref){
+
+    function getCurrentContext() {
+        $http.get($scope.properties.urlContext).success((data) => {
+            if (data.solicitudApoyoEducativo_ref) {
                 getModelSolicitudApoyoEducativo("../" + data.solicitudApoyoEducativo_ref.link);
                 getModelHermanos("../" + data.hermanos_ref.link);
+                getModelAutos("../" + data.autos_ref.link);
+                getModelBienesRaices("../" + data.bienesRaices_ref.link);
+
+                $scope.properties.fotoCalleCasa = addDataToDocuments(data.fotoCalleCasa_ref);
+                $scope.properties.fotoComedorCasa = addDataToDocuments(data.fotoComedorCasa_ref);
+                $scope.properties.fotoFachadaCasa = addDataToDocuments(data.fotoFachadaCasa_ref);
+                $scope.properties.fotoSalaCasa = addDataToDocuments(data.fotoSalaCasa_ref);
+
                 $scope.properties.lstDocumentos = [];
                 $scope.properties.autos = [];
                 let lstDoc = [];
@@ -26,63 +34,109 @@ function ($scope, $http) {
                     "contentType": null,
                     "id": null
                 }
-                
-                for(documento of data.lstDocumentos_ref){
-                    newValue.id = documento["id"] + "";
-                    lstDoc.push(angular.copy(newValue));
+
+                if (data.lstDocumentos_ref) {
+                    for (documento of data.lstDocumentos_ref) {
+                        newValue.id = documento["id"] + "";
+                        lstDoc.push(angular.copy(newValue));
+                    }
+
+                    $scope.properties.lstDocumentos = lstDoc;
                 }
-                
-                $scope.properties.lstDocumentos = lstDoc;
-                
-            } 
-        }).error((err)=>{
-            swal("Error","Error al obtener el context. " + err,"error");
+            }
+        }).error((err) => {
+            swal("Error", "Error al obtener el context. " + err, "error");
         });
     }
-    
-    function getModelSolicitudApoyoEducativo(url){
-        $http.get(url).success((data)=>{
-            if(data){
+
+    function addDataToDocuments(_document) {
+
+        let newValue = {
+            "filename": _document["fileName"],
+            "tempPath": null,
+            "contentType": _document["contentMimeType"],
+            "id": _document["id"] + ""
+        }
+
+        return newValue;
+    }
+
+    function getModelSolicitudApoyoEducativo(url) {
+        $http.get(url).success((data) => {
+            if (data) {
                 $scope.properties.solicitudApoyoEducativo = [];
                 $scope.properties.solicitudApoyoEducativo = data;
                 let links = $scope.properties.solicitudApoyoEducativo.links;
 
-                for(let link of links){
+                for (let link of links) {
                     getLazyRefModel(".." + link.href, link.rel);
                 }
             }
-        }).error((err)=>{
-            swal("Error","Error al obtener el model. " + err,"error");
+        }).error((err) => {
+            swal("Error", "Error al obtener el model. " + err, "error");
         });
     }
 
-    function getModelHermanos(url){
+    function getModelHermanos(url) {
         $scope.properties.hermanos = [];
-        $http.get(url).success((data)=>{
-            if(data){
+        $http.get(url).success((data) => {
+            if (data) {
                 $scope.properties.hermanos = data;
             }
-        }).error((err)=>{
+        }).error((err) => {
+            $scope.properties.hermanos = [];
             // swal("Error","Error al obtener el model. " + err,"error");
             console.log("hermanos vacío")
         });
-    } 
+    }
 
-    function getLazyRefModel(_url, _bdmFieldName){
-        $http.get(_url).success((data)=>{
-            $scope.properties.solicitudApoyoEducativo[_bdmFieldName] = data;
-        }).error((err)=>{
-            swal("Error","Error al obtener el model. " + err,"error");
+    function getModelAutos(url) {
+        $scope.properties.autos = [];
+        $http.get(url).success((data) => {
+            if (data) {
+                $scope.properties.autos = data;
+            }
+        }).error((err) => {
+            $scope.properties.autos = [];
+            // swal("Error","Error al obtener el model. " + err,"error");
+            console.log("autos vacío")
         });
     }
-    
-    $scope.$watch("properties.reloadTask", ()=>{
-        if($scope.properties.reloadTask){
-            getCurrentTaskId();
-        } 
+
+    function getModelBienesRaices(url) {
+        $scope.properties.bienesRaices = [];
+        $http.get(url).success((data) => {
+            if (data) {
+                $scope.properties.bienesRaices = data;
+            }
+        }).error((err) => {
+            $scope.properties.bienesRaices = [];
+            // swal("Error","Error al obtener el model. " + err,"error");
+            console.log("bienesRaices vacío")
+        });
+    }
+
+    function getLazyRefModel(_url, _bdmFieldName) {
+        $http.get(_url).success((data) => {
+            $scope.properties.solicitudApoyoEducativo[_bdmFieldName] = data;
+        }).error((err) => {
+            swal("Error", "Error al obtener el model. " + err, "error");
+        });
+    }
+
+    $scope.$watch("properties.taskId", (oldValue, newValue) => {
+        if ($scope.properties.taskId && oldValue !== newValue) {
+            getCurrentContext();
+        }
     });
-    
-     $scope.$watch("properties.url", ()=>{
+
+    $scope.$watch("properties.reloadTask", () => {
+        if ($scope.properties.reloadTask) {
             getCurrentTaskId();
+        }
+    });
+
+    $scope.$watch("properties.url", () => {
+        getCurrentTaskId();
     });
 }
