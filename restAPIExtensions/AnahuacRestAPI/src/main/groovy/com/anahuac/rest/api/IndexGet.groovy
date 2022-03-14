82e47fa99be28e6f2d8bc0f5fd891610c0dfaf5c
jusquer2 <74383444+jusquer2@users.noreply.github.com>
2022-03-14 09:24:03 -0700

    HubspotLog

--- restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/IndexGet.groovy
+++ restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/IndexGet.groovy
@@ -1120,6 +1120,16 @@ class IndexGet implements RestApiController {
 					 return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString());
 				}
 				break;
+				
+				case "getHubspotLog":
+				result = new LogDAO().getHubspotLog();
+				responseBuilder.withMediaType("application/json");
+				if (result.isSuccess()) {
+					 return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result.data).toString());
+				}else {
+					 return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString());
+				}
+				break;
 
 				case "getBachilleratoLog":
 				result = new LogDAO().getBachilleratoLog();
