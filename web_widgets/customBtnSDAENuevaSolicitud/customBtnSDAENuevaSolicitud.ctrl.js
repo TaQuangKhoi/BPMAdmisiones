c265a21732a5ee96d16c70f48110dba56c33a1f2
José García <josegarciar177@gmail.com>
2022-03-15 12:03:30 -0700

    se subieron ajustes para que  el proceso tome el tipo de moneda

--- web_widgets/customBtnSDAENuevaSolicitud/customBtnSDAENuevaSolicitud.ctrl.js
+++ web_widgets/customBtnSDAENuevaSolicitud/customBtnSDAENuevaSolicitud.ctrl.js
@@ -1,47 +1,41 @@
 function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageService, modalService) {
 
-  'use strict';
+    'use strict';
 
-  var vm = this;
+    var vm = this;
 
-  this.action = function action() {
-//   if ($scope.properties.url) {
-//       doRequest($scope.properties.action, $scope.properties.url);
-//     }
-    var url= "/portal/resource/app/sdae/avisoPrivacidadSDAE/content/"
-    window.location.replace(url); 
-  };
-
-  function doRequest(method, url, params) {
-    vm.busy = true;
-    var req = {
-      method: method,
-      url: url,
-      data: angular.copy($scope.properties.dataToSend),
-      params: params
+    this.action = function action() {
+        var url = "/portal/resource/app/aspiranteSDAE/nueva_solicitud_SDAE/content/?tipoMoneda=" + $scope.properties.tipoMoneda
+        window.location.replace(url);
     };
 
-    return $http(req)
-      .success(function(data, status) {
-        $scope.properties.dataFromSuccess = data;
-        $scope.properties.responseStatusCode = status;
-        $scope.properties.dataFromError = undefined;
-        notifyParentFrame({ message: 'success', status: status, dataFromSuccess: data, dataFromError: undefined, responseStatusCode: status});
-        if ($scope.properties.targetUrlOnSuccess && method !== 'GET') {
-          redirectIfNeeded();
-        }
-        closeModal($scope.properties.closeOnSuccess);
-      })
-      .error(function(data, status) {
-        $scope.properties.dataFromError = data;
-        $scope.properties.responseStatusCode = status;
-        $scope.properties.dataFromSuccess = undefined;
-        notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status});
-      })
-      .finally(function() {
-        vm.busy = false;
-      });
-  }
+    function doRequest(method, url, params) {
+        vm.busy = true;
+        var req = {
+            method: method,
+            url: url,
+            data: angular.copy($scope.properties.dataToSend),
+            params: params
+        };
 
-
+        return $http(req).success(function (data, status) {
+            $scope.properties.dataFromSuccess = data;
+            $scope.properties.responseStatusCode = status;
+            $scope.properties.dataFromError = undefined;
+            notifyParentFrame({ message: 'success', status: status, dataFromSuccess: data, dataFromError: undefined, responseStatusCode: status });
+            if ($scope.properties.targetUrlOnSuccess && method !== 'GET') {
+                redirectIfNeeded();
+            }
+            closeModal($scope.properties.closeOnSuccess);
+        })
+        .error(function (data, status) {
+            $scope.properties.dataFromError = data;
+            $scope.properties.responseStatusCode = status;
+            $scope.properties.dataFromSuccess = undefined;
+            notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status });
+        })
+        .finally(function () {
+            vm.busy = false;
+        });
+    }
 }
