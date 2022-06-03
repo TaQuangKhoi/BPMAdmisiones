7d9d2023dac455c23585a271dd0ea75abe6d512a
workchon <jesusangel6@hotmail.com>
2022-06-03 10:02:41 -0700

    actualizacion pantalla de rechazado

--- web_widgets/customBtnReactivarUsuario/customBtnReactivarUsuario.ctrl.js
+++ web_widgets/customBtnReactivarUsuario/customBtnReactivarUsuario.ctrl.js
@@ -3,6 +3,7 @@ function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageServi
     'use strict';
 
     var vm = this;
+    this.bloqueo = false;
 
     this.action = function action() {
         if ($scope.properties.action === 'Remove from collection') {
@@ -21,27 +22,44 @@ function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageServi
         } else if ($scope.properties.action === 'Close modal') {
             closeModal(true);
         } else if ($scope.properties.url) {
-
+            vm.bloqueo = true;
+            
             var existecambio = false;
             var isTransferencia = false;
-            if ($scope.properties.valoresSolicitante.catCampus === null) {
-                swal("¡Aviso!", "Debes seleccionar un campus", "warning");
+            if($scope.properties.valoresSolicitante.correoelectronico.includes("(rechazado)") ){
+                Swal.fire("¡Aviso!", "Este es un intento ya rechazado, no se puede volver a reactivar", "warning");
+                vm.bloqueo = false;
+            }
+            else if ($scope.properties.valoresSolicitante.catCampus === null) {
+               Swal.fire("¡Aviso!", "Debes seleccionar un campus", "warning");
+               vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLicenciatura === null) {
-                swal("¡Aviso!", "Debes seleccionar una licenciatura", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar una licenciatura", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.periodo === null || $scope.properties.valoresSolicitante.periodo === undefined) {
-                swal("¡Aviso!", "Debes seleccionar un período", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar un período", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLicenciatura.propedeutico && $scope.properties.valoresSolicitante.catPropedeutico === undefined) {
-                swal("¡Aviso!", "Debes seleccionar un curso propedéutico", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar un curso propedéutico", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLugarExamen === null) {
-                swal("¡Aviso!", "Debes seleccionar un lugar de examen", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar un lugar de examen", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLugarExamen.descripcion === "En un estado" && $scope.properties.valoresSolicitante.catEstadoExamen === null) {
-                swal("¡Aviso!", "Debes seleccionar un estado donde realizará  el examen", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar un estado donde realizará  el examen", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLugarExamen.descripcion === "En un estado" && $scope.properties.valoresSolicitante.ciudadExamen === null) {
-                swal("¡Aviso!", "Debes seleccionar una ciudad donde se realizará  el examen", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar una ciudad donde se realizará  el examen", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLugarExamen.descripcion === "En el extranjero (solo si vives fuera de México)" && $scope.properties.valoresSolicitante.catPaisExamen === null) {
-                swal("¡Aviso!", "Debes seleccionar un pais donde se realizará  el examen", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar un pais donde se realizará  el examen", "warning");
+                vm.bloqueo = false;
             } else if ($scope.properties.valoresSolicitante.catLugarExamen.descripcion === "En el extranjero (solo si vives fuera de México)" && $scope.properties.valoresSolicitante.ciudadExamenPais === null) {
-                swal("¡Aviso!", "Debes seleccionar una ciudad donde se realizará  el examen", "warning");
+                Swal.fire("¡Aviso!", "Debes seleccionar una ciudad donde se realizará  el examen", "warning");
+                vm.bloqueo = false;
+            } else if((parseInt($scope.properties.valoresSolicitante.countrechazos) + 1 ) >= 2){
+                 Swal.fire("¡Aviso!", "No se le puede dar una tercera oportunidad al aspirante", "warning");
+                 vm.bloqueo = false;
             } else {
                 $scope.properties.JSONTransferencia.caseid = $scope.properties.valoresSolicitante.caseid;
                 $scope.properties.JSONTransferencia.licenciatura = $scope.properties.valoresSolicitante.catLicenciatura.persistenceId;
@@ -82,10 +100,7 @@ function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageServi
                 console.log($scope.properties.JSONTransferencia);
                 $scope.properties.estadoSeleccionado = undefined;
                 doRequest($scope.properties.action, $scope.properties.url);
-
-
-
-
+                
             }
         }
     };
@@ -196,6 +211,7 @@ function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageServi
             })
             .finally(function() {
                 vm.busy = false;
+                vm.bloqueo = false;
             });
     }
 
