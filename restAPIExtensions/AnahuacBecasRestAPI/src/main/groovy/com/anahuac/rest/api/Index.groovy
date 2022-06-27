97241e6e3c7a81e1da62d68d222492411e614f52
workchon <jesusangel6@hotmail.com>
2022-06-27 15:10:54 -0700

    new request

--- restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/Index.groovy
+++ restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/Index.groovy
@@ -254,6 +254,15 @@ class Index implements RestApiController {
 					}
 				break;
 				
+				case "selectBandejaMaestra":
+					result = new ListadoDAO().selectBandejaMaestra(parameterP, parameterC, jsonData, context);
+					if (result.isSuccess()) {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
+					}else {
+						return buildResponse(responseBuilder, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,  new JsonBuilder(result).toString())
+					}
+				break;
+				
 				case "getSolicitudApoyoByCaseId":
 					result = new ListadoDAO().getSolicitudApoyoByCaseId(jsonData, context);
 					if (result.isSuccess()) {
