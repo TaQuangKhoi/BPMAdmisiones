349ee47f5bd6ecc9c5163198e82bd38ab1d1b4fb
Jesus Osuna <jesusangel6@hotmail.com>
2022-10-04 18:30:19 -0700

    cambio en lo de jubilado del reporte psicometrico

--- restAPIExtensions/DocRestAPI/src/main/groovy/com/anahuac/rest/api/DAO/PDFDocumentDAO.groovy
+++ restAPIExtensions/DocRestAPI/src/main/groovy/com/anahuac/rest/api/DAO/PDFDocumentDAO.groovy
@@ -108,7 +108,7 @@ class PDFDocumentDAO {
 						columns.put("universidadPadre", isNullOrBlanck(it?.categresoanahuac_pid.toString())== "N/A"?"No":"Si" );
 					} else {
 						columns.put("nombrePadre", isNullOrBlanck(it?.nombre.toString()));
-						columns.put("ocupacionPadre", isNullOrBlanck(it?.puesto.toString())+"  ${ isNullOrBlanck(it?.puesto.toString()) !='N/A'? (it?.jubilado.toString().equals('t')?'(jubilado)':'' ) :''} ");
+						columns.put("ocupacionPadre", isNullOrBlanck(it?.puesto.toString())+"  ${ isNullOrBlanck(it?.vive.toString()).equals('Sí')? (it?.jubilado.toString().equals('t')?'(jubilado)':'' ) :''} ");
 						columns.put("empresaPadre", isNullOrBlanck(it?.empresatrabaja.toString()));
 						columns.put("universidadPadre", isNullOrBlanck(it?.categresoanahuac_pid.toString())== "N/A"?"No":"Si" );
 					}
@@ -128,7 +128,7 @@ class PDFDocumentDAO {
 						columns.put("universidadMadre", isNullOrBlanck(it?.categresoanahuac_pid.toString())== "N/A"?"No":"Si" );
 					} else {
 						columns.put("nombreMadre", isNullOrBlanck(it?.nombre.toString()));
-						columns.put("ocupacionMadre", isNullOrBlanck(it?.puesto.toString())+ " ${ isNullOrBlanck(it?.puesto.toString()) !='N/A'? (it?.jubilado.toString()=='t'?'(jubilado)':'' ) :''} ");
+						columns.put("ocupacionMadre", isNullOrBlanck(it?.puesto.toString())+ " ${ isNullOrBlanck(it?.vive.toString()).equals('Sí')? (it?.jubilado.toString()=='t'?'(jubilado)':'' ) :''} ");
 						columns.put("empresaMadre", isNullOrBlanck(it?.empresatrabaja.toString()));
 						columns.put("universidadMadre", isNullOrBlanck(it?.categresoanahuac_pid.toString())== "N/A"?"No":"Si" );
 					}
@@ -137,9 +137,17 @@ class PDFDocumentDAO {
 				}
 				
 				if(it?.istutor.toString().equals("t")  && !familiares[2]) {
+					
 					columns.put("nombreTutor", isNullOrBlanck(it?.nombre.toString()));
-					columns.put("ocupacionTutor", isNullOrBlanck(it?.puesto.toString())+"  ${ isNullOrBlanck(it?.puesto.toString()) !='N/A'? (it?.jubilado.toString()=='t'?'(jubilado)':'' ) :''}");
-					columns.put("empresaTutor", isNullOrBlanck(it?.empresatrabaja.toString()));
+					
+					if(it?.trabaja.toString().equals("No")) {
+						columns.put("ocupacionTutor", it?.jubilado.toString().equals("t") ?isNullOrBlanck("Jubilado") : isNullOrBlanck("No trabaja"));
+						columns.put("empresaTutor", isNullOrBlanck("No trabaja"));
+					}else {
+						columns.put("ocupacionTutor", isNullOrBlanck(it?.puesto.toString())+"  ${ (it?.jubilado.toString()=='t'?'(jubilado)':'')  }");
+						columns.put("empresaTutor", isNullOrBlanck(it?.empresatrabaja.toString()));
+					}
+					
 					columns.put("universidadTutor", isNullOrBlanck(it?.categresoanahuac_pid.toString())== "N/A"?"No":"Si" );
 					familiares[2] = true;
 				}
