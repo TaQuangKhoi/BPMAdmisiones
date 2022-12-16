7a78c5fbcc84b307dd884bcd1306526a15d5fdf3
José García <josegarciar177@gmail.com>
2022-12-16 12:30:56 -0700

    se subiero ajustes al BDM y para el manejo de los datos de banner

---
+++ restAPIExtensions/AnahuacBecasRestAPI/src/main/groovy/com/anahuac/rest/api/DB/BannerWSInfo.groovy
@@ -0,0 +1,40 @@
+package com.anahuac.rest.api.DB
+
+class BannerWSInfo {
+	private String pago_de_propedeutico;
+	private String admitido_a_medicina;
+	private String pago_de_inscripcion;
+	private String programa_periodo_campus;
+	private String carga_de_materias;
+	
+	public String getPago_de_propedeutico() {
+		return pago_de_propedeutico;
+	}
+	public void setPago_de_propedeutico(String pago_de_propedeutico) {
+		this.pago_de_propedeutico = pago_de_propedeutico;
+	}
+	public String getAdmitido_a_medicina() {
+		return admitido_a_medicina;
+	}
+	public void setAdmitido_a_medicina(String admitido_a_medicina) {
+		this.admitido_a_medicina = admitido_a_medicina;
+	}
+	public String getPago_de_inscripcion() {
+		return pago_de_inscripcion;
+	}
+	public void setPago_de_inscripcion(String pago_de_inscripcion) {
+		this.pago_de_inscripcion = pago_de_inscripcion;
+	}
+	public String getPrograma_periodo_campus() {
+		return programa_periodo_campus;
+	}
+	public void setPrograma_periodo_campus(String programa_periodo_campus) {
+		this.programa_periodo_campus = programa_periodo_campus;
+	}
+	public String getCarga_de_materias() {
+		return carga_de_materias;
+	}
+	public void setCarga_de_materias(String carga_de_materias) {
+		this.carga_de_materias = carga_de_materias;
+	}
+}
