ecc755f4d870c06e0f57b557ec3a98fe4d915a9e
Jesus Osuna <jesusangel6@hotmail.com>
2023-03-15 12:23:08 -0700

    Cambio en las fotos

--- restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/Security/SecurityFilter.groovy
+++ restAPIExtensions/AnahuacRestAPI/src/main/groovy/com/anahuac/rest/api/Security/SecurityFilter.groovy
@@ -2000,7 +2000,7 @@ class SecurityFilter {
 		    }
 		    break;
 		case "getUserFoto":
-		allow=bonitaRolFilter(context,"TI SERUA")
+		allow=bonitaRolFilter(context,"ADMISIONES")
 			if(allow){break;}
 			break;
 		}
